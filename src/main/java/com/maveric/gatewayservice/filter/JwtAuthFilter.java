0d4ca0935b6072ab3bcf1d96937d8ad8e043c8ce
Ram Krishnan <ramkrishnane@maveric-systems.com>
2022-09-14 16:10:24 +0530

    Added crossorigin annotation for angular

--- src/main/java/com/maveric/gatewayservice/filter/JwtAuthFilter.java
+++ src/main/java/com/maveric/gatewayservice/filter/JwtAuthFilter.java
@@ -1,19 +1,20 @@
 package com.maveric.gatewayservice.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.maveric.gatewayservice.dto.ErrorDto;
 import com.maveric.gatewayservice.dto.GateWayRequestDto;
 import com.maveric.gatewayservice.dto.GateWayResponseDto;
-import com.maveric.gatewayservice.feignconsumer.JwtUtil;
 import com.maveric.gatewayservice.util.JwtAuthUtil;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.http.*;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -25,11 +26,13 @@ public class JwtAuthFilter implements GatewayFilter {
 
     @Autowired
     private RouterValidator routerValidator;
-    @Autowired
-    private JwtUtil jwtUtil;
+
 
     @Autowired
     private JwtAuthUtil jwtAuthUtil;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -55,7 +58,7 @@ public class JwtAuthFilter implements GatewayFilter {
             }
             catch(Exception e)
             {
-                System.out.println("Exception during validation of token-"+e);
+                return this.onError(exchange, "Exception in validating the token", HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
         return chain.filter(exchange);
@@ -65,8 +68,19 @@ public class JwtAuthFilter implements GatewayFilter {
     /*PRIVATE*/
 
     private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        DataBufferFactory dataBufferFactory = exchange.getResponse().bufferFactory();
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(httpStatus);
+        try {
+            response.getHeaders().add("Content-Type", "application/json");
+            ErrorDto errorDto = new ErrorDto(String.valueOf(httpStatus.value()), err);
+            byte[] byteData = objectMapper.writeValueAsBytes(errorDto);
+            return response.writeWith(Mono.just(byteData).map(t -> dataBufferFactory.wrap(t)));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
         return response.setComplete();
     }
 
@@ -86,12 +100,7 @@ public class JwtAuthFilter implements GatewayFilter {
 
     private GateWayResponseDto validateToken(String token) {
         try {
-            //ResponseEntity<GateWayResponseDto> responseEntity = jwtUtil.validateToken(gateWayRequestDto);
             GateWayResponseDto responseEntity = jwtAuthUtil.validateToken(token);
-            /*HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            HttpEntity<GateWayRequestDto> entity = new HttpEntity<GateWayRequestDto>(gateWayRequestDto,headers);
-            GateWayResponseDto responseEntity = restTemplate.exchange("http://localhost:3000/api/v1/auth/validate", HttpMethod.POST, entity, GateWayResponseDto.class).getBody(); */
             System.out.println("Success Validation of token->"+responseEntity.isResponse());
             return responseEntity;
         }
